c8dae1fb383c2a25c1aa759575bb3236c875a6f0
wibus-wee <1596355173@qq.com>
2023-09-13 23:28:58 +0800

    feat: add sendNotifiaction util

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,38 @@
-import { activator } from "./modules";
+function transformToString(obj: any) {
+  if (typeof obj === "object") {
+    return JSON.stringify(obj);
+  }
+  return obj;
+}
 
+/**
+ * 构建 Surge 响应体
+ *
+ * @param props 响应体属性
+ * @description 该函数将会自动将对象转换为 JSON 字符串，因此你可以直接传入对象
+ */
 export function buildResponse(props: { [x: string]: any; body?: any }) {
   for (let i in props) {
-    if (typeof props[i] === "object") {
-      props[i] = JSON.stringify(props[i]);
-    }
+    props[i] = transformToString(props[i]);
   }
   $done({
     response: {
       ...props,
     },
   });
+}
+
+/**
+ * 发送通知
+ *
+ * @param title 标题
+ * @param subtitle 副标题
+ * @param body 内容
+ * @description 该函数将会自动将对象转换为 JSON 字符串，因此你可以直接传入对象
+ */
+export function sendNotification(title: any, subtitle: any, body: any) {
+  title = transformToString(title);
+  subtitle = transformToString(subtitle);
+  body = transformToString(body);
+  $notification.post(title, subtitle, body);
 }
