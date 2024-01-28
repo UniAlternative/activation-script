f9d29056f454a8f052cc0765e40a9f11761a20bf
wibus-wee <1596355173@qq.com>
2024-01-28 13:16:02 +0800

    fix(generator): configuration patcher error

--- src/generator.ts
+++ src/generator.ts
@@ -27,7 +27,7 @@ ${_scripts
 
 function addConfig(module: string, base: string) {
   console.log(`[I] Generating config for ${module}... Base: ${base}`);
-  
+
   const url = new URL(base);
   const hostname = url.hostname;
   const parts = hostname.split(".");
@@ -91,6 +91,155 @@ function generateScriptConfig(echo = false) {
   return;
 }
 
+async function patch() {
+  let configPath = process.cwd();
+  // 询问配置文件所在位置。1. 当前目录 2. /Users/<username>/Library/Application Support/Surge/Profiles 3. 自定义
+  await prompts([
+    {
+      type: "select",
+      name: "config",
+      message: "Where is your config file?",
+      choices: [
+        { title: "Current directory", value: "current" },
+        {
+          title: "Surge config directory",
+          value: "surge",
+        },
+        { title: "Custom", value: "custom" },
+      ],
+    },
+  ])
+    .then(async (res: { config: string }) => {
+      if (res.config === "current") {
+        configPath = process.cwd();
+      } else if (res.config === "surge") {
+        configPath = path.join(
+          process.env.HOME as string,
+          "Library/Application Support/Surge/Profiles"
+        );
+      } else {
+        await prompts([
+          {
+            type: "text",
+            name: "config",
+            message: "Please input your config file path",
+          },
+        ])
+          .then((res: { config: string }) => {
+            configPath = res.config;
+          })
+          .catch((err: any) => {
+            console.log(err);
+          });
+      }
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
+  let configFiles = Array<string>();
+  fs.readdirSync(configPath).forEach((file) => {
+    if (file.endsWith(".conf")) {
+      configFiles.push(file);
+    }
+  });
+  if (configFiles.length === 0) {
+    console.log("No config file found.");
+    return;
+  }
+
+  prompts([
+    {
+      type: "select",
+      name: "config",
+      message: "Which config file do you want to use?",
+      choices: configFiles.map((file) => {
+        return { title: file, value: file };
+      }),
+    },
+  ])
+    .then((res: { config: string }) => {
+      let newConfig = "";
+      const config = fs.readFileSync(
+        path.join(configPath, res.config),
+        "utf-8"
+      );
+      const regexMITM = /\[MITM\]/g;
+      const regexScript = /\[Script\]/g;
+
+      if (!regexMITM.test(config)) {
+        console.error(
+          "❌ MITM not found. You should config MITM in Surge first."
+        );
+        return;
+      }
+      const regexMITMHostnames = /hostname = (.*)/g;
+      const oldHostnames = regexMITMHostnames.exec(config) as RegExpExecArray;
+      console.log("[*] OldHostnames", oldHostnames[1]);
+      // 去重，合并
+      const newHostnames = Array.from(
+        new Set([...oldHostnames[1].split(", "), ...hostnames])
+      );
+      console.log("[*] NewHostnames", newHostnames);
+
+      newConfig = config.replace(
+        regexMITMHostnames,
+        `hostname = ${newHostnames.join(", ")}`
+      ); // 替换掉原本的 hostname
+
+      fs.writeFileSync(path.join(configPath, res.config), newConfig);
+      // let oldScriptContent = "";
+      const regexScriptBeforeLastQuoteContent = /\[Script\]([\s\S]*?)\[/g;
+      const regexScriptContent = /\[Script\]([\s\S]*)/g;
+      let isScriptHasContentBeforeLastQuote = false;
+      if (regexScriptBeforeLastQuoteContent.test(config)) {
+        isScriptHasContentBeforeLastQuote = true;
+      }
+      // const oldScriptBeforeLastQuoteContent =
+      //   regexScriptBeforeLastQuoteContent.exec(
+      //     newConfig
+      //   ) as RegExpExecArray | null;
+      // if (oldScriptBeforeLastQuoteContent) {
+      //   oldScriptContent = oldScriptBeforeLastQuoteContent[1];
+      // } else {
+      //   const oldScriptContentNoLastQuote = regexScriptContent.exec(
+      //     newConfig
+      //   ) as RegExpExecArray | null;
+      //   if (oldScriptContentNoLastQuote) {
+      //     oldScriptContent = oldScriptContentNoLastQuote[1];
+      //   } else {
+      //     console.error(
+      //       "❌ Script not found. You should config Script in Surge first."
+      //     );
+      //     return;
+      //   }
+      // }
+      // console.log("[*] OldScriptContent", oldScriptContent);
+
+      let newScriptContent = Script(scripts);
+      // newScriptContent 去掉 [Script]
+      // newScriptContent = newScriptContent.replace("[Script]\n", "");
+      console.log("[*] NewScriptContent", newScriptContent);
+
+      // newConfig = newConfig.replace(
+      //   regexScriptContent,
+      //   `[Script]${mergedScriptContent}`
+      // );
+
+      newConfig = newConfig.replace(
+        isScriptHasContentBeforeLastQuote
+          ? regexScriptBeforeLastQuoteContent
+          : regexScriptContent,
+        `${newScriptContent}`
+      );
+
+      fs.writeFileSync(path.join(configPath, res.config), newConfig);
+      console.log("Config file fixed.");
+    })
+    .catch((err: any) => {
+      console.log(err);
+    });
+}
+
 async function action(str: any, options: any) {
   generateScriptConfig(Object.keys(str).length === 0);
   if (str.out) {
@@ -101,127 +250,6 @@ async function action(str: any, options: any) {
     );
     console.log("Config file generated.");
   }
-  if (str.fix) {
-    let configPath = process.cwd();
-    // 询问配置文件所在位置。1. 当前目录 2. /Users/<username>/Library/Application Support/Surge/Profiles 3. 自定义
-    await prompts([
-      {
-        type: "select",
-        name: "config",
-        message: "Where is your config file?",
-        choices: [
-          { title: "Current directory", value: "current" },
-          {
-            title: "Surge config directory",
-            value: "surge",
-          },
-          { title: "Custom", value: "custom" },
-        ],
-      },
-    ])
-      .then(async (res: { config: string }) => {
-        if (res.config === "current") {
-          configPath = process.cwd();
-        } else if (res.config === "surge") {
-          configPath = path.join(
-            process.env.HOME as string,
-            "Library/Application Support/Surge/Profiles"
-          );
-        } else {
-          await prompts([
-            {
-              type: "text",
-              name: "config",
-              message: "Please input your config file path",
-            },
-          ])
-            .then((res: { config: string }) => {
-              configPath = res.config;
-            })
-            .catch((err: any) => {
-              console.log(err);
-            });
-        }
-      })
-      .catch((err: any) => {
-        console.log(err);
-      });
-    let configFiles = Array<string>();
-    fs.readdirSync(configPath).forEach((file) => {
-      if (file.endsWith(".conf")) {
-        configFiles.push(file);
-      }
-    });
-    if (configFiles.length === 0) {
-      console.log("No config file found.");
-      return;
-    }
-
-    prompts([
-      {
-        type: "select",
-        name: "config",
-        message: "Which config file do you want to use?",
-        choices: configFiles.map((file) => {
-          return { title: file, value: file };
-        }),
-      },
-    ])
-      .then((res: { config: string }) => {
-        let newConfig = "";
-        const config = fs.readFileSync(
-          path.join(configPath, res.config),
-          "utf-8"
-        );
-        const regexMITM = /\[MITM\]/g;
-        const regexScript = /\[Script\]/g;
-
-        if (!regexMITM.test(config)) {
-          console.error(
-            "❌ MITM not found. You should config MITM in Surge first."
-          );
-          return;
-        }
-        const regexMITMHostnames = /hostname = (.*)/g;
-        const oldHostnames = regexMITMHostnames.exec(config) as RegExpExecArray;
-        const newHostnames = Array.from(
-          new Set([...oldHostnames, ...hostnames])
-        );
-        console.log(newHostnames);
-
-        newConfig = config.replace(
-          regexMITMHostnames,
-          `${newHostnames.join(", ")}`
-        );
-
-        fs.writeFileSync(path.join(configPath, res.config), newConfig);
-        if (!regexScript.test(config)) {
-          // 在最后加一行 [Script]
-          newConfig = config + "\n[Script]";
-        }
-
-        // 读取 [Script] 到下一个 [ 开始的距离（如果有的话）接着就缓存原本有的 Script，再把整一段给替换掉，再把缓存的 Script 写进去
-        const regexScriptContent = /\[Script\]([\s\S]*?)\[/g;
-        const oldScriptContent = regexScriptContent.exec(
-          newConfig
-        ) as RegExpExecArray | null;
-        const newScriptContent = Script(scripts);
-        console.log(newScriptContent);
-
-        if (oldScriptContent) {
-          newConfig = newConfig.replace(regexScriptContent, newScriptContent);
-        } else {
-          newConfig = newConfig + newScriptContent;
-        }
-
-        fs.writeFileSync(path.join(configPath, res.config), newConfig);
-        console.log("Config file fixed.");
-      })
-      .catch((err: any) => {
-        console.log(err);
-      });
-  }
-
   return;
 }
 
@@ -335,5 +363,7 @@ program
   .action(action);
 
 program.command("inject").description("inject activator").action(inject);
+
+program.command("patch").description("patch config").action(patch);
 
 program.parse();
