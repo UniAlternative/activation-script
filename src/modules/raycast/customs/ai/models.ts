17622a20a0ce6bdab2d9d7ca0dbfa9a0a613670c
wibus-wee <1596355173@qq.com>
2024-02-01 19:43:43 +0800

    Add raycastAiModels function to handle AI models

---
+++ src/modules/raycast/customs/ai/models.ts
@@ -0,0 +1,29 @@
+import { buildResponse } from "../../../../utils";
+import { RAYCAST_DEFAULT_MODELS, RAYCAST_AI_SERVICE_PROVIDERS, RAYCAST_GEMINI_PRO_ONLY_MODELS } from "../../constants";
+
+/**
+ * @url https://backend.raycast.com/api/v1/ai/models
+ */
+export function raycastAiModels() {
+  let data = JSON.parse($response.body);
+  let default_models;
+  switch (config.ai.type) {
+    case "openai":
+      default_models = RAYCAST_DEFAULT_MODELS;
+      break;
+    case "gemini":
+      default_models = RAYCAST_GEMINI_PRO_ONLY_MODELS;
+      break;
+    default:
+      default_models = RAYCAST_DEFAULT_MODELS;
+      break;
+  }
+  buildResponse({
+    headers: $response.headers,
+    body: {
+      ...data,
+      default_models,
+      models: RAYCAST_AI_SERVICE_PROVIDERS,
+    }
+  })
+}
