d382d8ece122c1a1d334754ae2757c9cc42adab0
wibus-wee <1596355173@qq.com>
2024-02-02 20:54:03 +0800

    perf: support more build func

--- types.d.ts
+++ types.d.ts
@@ -2,14 +2,15 @@ declare global {
   var $request: {
     url: string;
     method: string;
-    headers: any;
+    headers: Record<string, string>;
     body: string;
+    status: number;
   };
   var $done: (props?: any) => void;
   var $notification: any;
   var $httpClient: any;
   var $response: {
-    headers: any;
+    headers: Record<string, string>;
     body: string;
   };
 }
