50e1bf79fd0fa223e7fc2b50d9105c0410041c0c
wibus-wee <1596355173@qq.com>
2024-02-12 20:57:05 +0800

    fix: use jsdelivr as external url

--- src/generator.ts
+++ src/generator.ts
@@ -7,7 +7,7 @@ import { inject } from "./generator/inject";
 
 const hostnames = Array<string>();
 const scripts = Array<{ name: string; pattern: string; type: string }>();
-const externalUrl = `https://raw.githubusercontent.com/wibus-wee/activation-script/gh-pages`
+const externalUrl = `https://fastly.jsdelivr.net/gh/wibus-wee/activation-script@gh-pages`
 
 const MITM = (hostnames: any[], external = false) => {
   return `
