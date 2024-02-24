0ee82b102a6713788c8c52beeb1288412658eb30
wibus-wee <1596355173@qq.com>
2024-02-24 20:00:22 +0800

    perf(launch): URL matching logic in launch

--- packages/core/src/launch.ts
+++ packages/core/src/launch.ts
@@ -1,62 +1,114 @@
+import type { ActivatorObjFunc } from '@as/shared'
 import { buildResponse, httpClient } from '@as/shared'
-import { DashboardRoute } from '@as/dashboard'
 import { activator } from '@as/modules'
 
 const url = $request.url.split('?')[0]
 
 /**
- * Determine whether the URL matches the base
+ * 检查 url 是否匹配 base 配置
  */
 export function isMatchBase(url: string, base: string | string[]) {
   if (Array.isArray(base)) {
     for (const item of base) {
-      if (url.includes(item))
+      if (isMatchBase(url, item))
         return true
     }
     return false
   }
-  return url.includes(base)
+  url = url.replace(/\/$/, '')
+  base = base.replace(/\/$/, '')
+  if (url === base)
+    return true
+  else if (base.includes('*') && url.includes(base.replace('*', '').replace(/\/$/, '')))
+    return true
+  else
+    return false
 }
 
 /**
  * Automatic execution of the corresponding function according to the URL
+ * 自动执行对应的函数
+ * @description This will match the URL according to the base of the module function, and if it matches, it will execute the func of the module function
  */
 export function launch() {
-  if (url.includes('as.as')) // dashboard route
-    return DashboardRoute(url)
+  console.log(`[activator] ${url}`)
+
+  /**
+   * 匹配模块函数
+   * @description 这会根据模块函数的 base 属性来匹配 url，如果匹配成功则执行模块函数的 func 属性
+   *
+   * @param moduleFunc 模块函数
+   * @returns 匹配结果
+   *
+   */
+  function matchModuleFunc(moduleFunc: ActivatorObjFunc) {
+    console.log(`[activator] matchModuleFunc: ${moduleFunc.base} | ${isMatchBase(url, moduleFunc.base)}`)
+    // 处理 * 通配符
+    if (isMatchBase(url, moduleFunc.base))
+      return moduleFunc.func()
+    // 不然就是要完全匹配
+    else if (url.replace(/\/$/, '') === moduleFunc.base.replace(/\/$/, '')) // 去掉末尾的 / 后再匹配
+      return moduleFunc.func()
+  }
+
+  /**
+   * 处理模块函数
+   * @description 这会根据模块函数的类型来执行对应的处理
+   *
+   * @param moduleFunc 模块函数
+   * @returns 匹配结果
+   *
+   */
+  function handleModuleFunc(moduleFunc: ActivatorObjFunc) {
+    if (typeof moduleFunc === 'object') {
+      const match = matchModuleFunc(moduleFunc)
+      if (match)
+        return match
+    }
+  }
+
   for (const module in activator) {
-    if (isMatchBase(url, activator[module].base)) {
-      for (const key in activator[module]) {
-        if (key === 'base')
+    const moduleItem = activator[module]
+
+    if (isMatchBase(url, moduleItem.base)) { // 检查 url 是否匹配 module 中配置的 base（利用 includes）
+      for (const key in moduleItem) { // 遍历 module 中的配置（base, activate, validate, customs）
+        const moduleItemOptions = moduleItem[key as keyof typeof moduleItem]
+
+        if (key === 'base') // 自然是不需要的
           continue
-        if (Array.isArray((activator[module] as any)[key])) {
-          for (const custom of (activator[module] as any)[key]) {
-            if (
-              custom.base === '*'
-              && url.startsWith((activator[module] as any).base)
-            )
-              return custom.func()
-            else if (url === `${activator[module].base}/${custom.base}`)
-              return custom.func()
+
+        // 如果配置是数组（意味着有多个配置）这只会在 customs 中出现
+        if (typeof moduleItemOptions === 'object' && Array.isArray(moduleItemOptions)) {
+          for (const custom of moduleItemOptions as ActivatorObjFunc[]) {
+            const match = handleModuleFunc({
+              ...custom,
+              base: `${moduleItem.base}/${custom.base.replace(/^\//, '')}`,
+            })
+            if (match)
+              return match
           }
           continue
         }
-        if (typeof (activator[module] as any)[key] === 'object') {
-          // *
-          if ((activator[module] as any)[key].base === '*')
-            return (activator[module] as any)[key].func()
 
-          if (
-            url
-            === `${activator[module].base}/${(activator[module] as any)[key].base}`
-          )
-            return (activator[module] as any)[key].func()
-        }
-        else if (!url.includes(`${activator[module].base}/${key}`)) {
+        // 如果配置是对象（意味着只有一个配置）这只会在 activate 和 validate 中出现
+        if (typeof moduleItemOptions === 'object') {
+          const match = handleModuleFunc(moduleItemOptions)
+          if (match)
+            return match
           continue
         }
-        if (typeof (activator[module] as any)[key] === 'function')
-          return (activator[module] as any)[key]()
+
+        // 如果 url 不包含 module.base/key 则跳过，不用管后面的了
+        if (!url.includes(`${moduleItem.base}/${key}`))
+          continue
+
+        // 如果配置是函数，这个时候其实就没有什么特殊情况了，所以直接执行
+        if (typeof moduleItemOptions === 'function')
+          return moduleItemOptions()
+
+        // 如果配置是字符串，几乎没有使用过，也算直接返回吧
+        if (typeof moduleItemOptions === 'string')
+          return buildResponse({ body: moduleItemOptions })
       }
     }
   }
@@ -67,28 +119,29 @@ export function launch() {
 
 function returnDefaultResponse() {
   console.log(
-    `[activator] returnDefaultResponse: ${url} - ${$request.method.toLowerCase()}`,
+    `[activator] returnDefaultResponse: [${$request.method}] -> ${url}`,
   )
-  // @ts-expect-error 这个地方无法通过类型检查
-  httpClient[$request.method.toLowerCase()](
+
+  httpClient[$request.method.toLowerCase() as keyof typeof httpClient](
     {
       url: $request.url,
       headers: $request.headers,
     },
     (err: any, response: any, _data: any) => {
-      if (!_data) {
-        console.log('returnDefaultResponse: _data is null', err)
-        buildResponse({
-          status: 500,
-          body: {},
-        })
+      if (err) {
+        console.log(err)
+        return buildResponse({ status: 500, body: err })
       }
+
+      if (!_data)
+        console.log('No data returned')
+
       const res = {
         status: response.status,
         headers: response.headers,
         body: _data,
       }
-      buildResponse(res)
+      return buildResponse(res)
     },
   )
 }
