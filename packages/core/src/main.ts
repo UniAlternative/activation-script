50c0af4787a3b0312dd984fb4caa9146c58c4c2e
wibus-wee <1596355173@qq.com>
2024-03-03 10:40:53 +0800

    perf(core): reduce package size

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,6 +1,5 @@
-import packageJson from '../package.json'
 import { launch } from './launch'
 
 console.log(`===== Activator Script Handler =====`)
-console.log(`===== Author: @wibus-wee | Version: ${packageJson.version} | Commit: ${COMMIT_HASH?.slice(0, 7) || 'main'} =====`)
+console.log(`===== Author: @wibus-wee | Version: ${CORE_VERSION} | Commit: ${COMMIT_HASH?.slice(0, 7) || 'main'} =====`)
 $done(launch())
