87f01c48be2a5ec626466d44ac8750ea4f2ef8b1
wibus-wee <1596355173@qq.com>
2024-06-23 10:32:01 +0800

    feat: jwt sign token

--- packages/modules/locafe/notchnook/verify.ts
+++ packages/modules/locafe/notchnook/verify.ts
@@ -1,12 +1,20 @@
 import { ResponseDone } from '@as/shared'
+import jwt from 'jsonwebtoken'
 
 /**
  * @url https://lo.cafe/api/notchnook-verify-key
  */
 export function locafeNotchNook() {
+  const token = jwt.sign({
+    valid: false,
+    iat: Math.floor(Date.now() / 1000),
+  }, 'secret', {
+    algorithm: 'RS256',
+  })
+
   return ResponseDone({
     body: {
-      valid: true,
+      token,
     },
   })
 }
